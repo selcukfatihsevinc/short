579a4e9dad4b2ca57894bb00b4f21d8190b42381
Selcuk Fatih Sevinc <selcukfatihsevinc@Selcuks-MacBook-Pro.local>
2016-06-01 14:40:09 +0300

    update

--- models/ShortURL.js
+++ models/ShortURL.js
@@ -1,26 +1,17 @@
+var wrapper = require('./prototype.js');
 
-/**
- * @model ShortURL
- */
+exports.ShortURL = function(mongoose) {
+  var Schema   = mongoose.Schema;
+  var ObjectId = Schema.ObjectId;
 
-var options
-  , ShortURLSchema
-  , mongoose = require('mongoose')
-  , wrapper = require('./prototype.js')
-  , Schema = mongoose.Schema
-  , ObjectId = Schema.ObjectId;
+  ShortURLSchema = new Schema({
+    id         : { type : ObjectId },
+    URL        : { type : String, unique: false },
+    hash       : { type : String, unique: true },
+    hits       : { type : Number, default: 0 },
+    data       : { type : Schema.Types.Mixed },
+    created_at : { type : Date, default: Date.now }
+  });
 
-options = { 
-  versionKey : false
+  return new wrapper.Model(mongoose.model('ShortURL', ShortURLSchema));
 };
-
-ShortURLSchema = new Schema({
-  id         : { type : ObjectId },
-  URL        : { type : String, unique: false },
-  hash       : { type : String, unique: true },
-  hits       : { type : Number, default: 0 },
-  data       : { type : Schema.Types.Mixed },
-  created_at : { type : Date, default: Date.now },
-}, options);
-
-exports.ShortURL = new wrapper.Model(mongoose.model('ShortURL', ShortURLSchema));
